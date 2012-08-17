eb4ddc25ffb46ff48765a7ec34a32449e0245f1c
Eduardo Lanchares <edulanchi@gmail.com>
2012-08-17 08:47:42 +0200

    Now we can get songs from artists

--- lib/echorb/artist.rb
+++ lib/echorb/artist.rb
@@ -1,22 +1,11 @@
-require 'net/http'
+require "echorb/base"
 
 module Echorb
-  class Artist
-    attr_reader :id, :name
+  class Artist < Echorb::Base
+    attr_reader :name, :familiarity, :hotttnesss, :terms, :years_active
 
-    def initialize id, name
-      @id = id
-      @name = name
-    end
-
-    def self.search name
-      search_params = { :name => name }
-      search_uri = URI("http://developer.echonest.com/api/v4/artist/search")
-      search_uri.query = URI.encode_www_form(search_params)
-
-      response = Net::HTTP.get_response(search_uri)
-      result = JSON.parse(response.body)
-      result["response"]["artists"]
+    def songs
+      @songs ||= @attrs[:songs].collect { |song| Song.from_response(song) }
     end
   end
 end
