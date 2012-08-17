692c97ba9ef72e412f392d3cd69ed9b3ce9aa31b
Eduardo Lanchares <edulanchi@gmail.com>
2012-08-17 08:46:28 +0200

    Added error classes

--- lib/echorb.rb
+++ lib/echorb.rb
@@ -1,11 +1,38 @@
 require "echorb/version"
-require "echorb/artist"
+require "echorb/client"
+
+def Echorb(api_key)
+  Echorb::Client.new(api_key)
+end
 
 module Echorb
-  API_VERSION = "v4"
-  API_ENDPOINT = "http://developer.echonest.com/api/#{API_VERSION}"
+  class Error < RuntimeError
+    attr_reader :code
 
-  def self.compose_url resource, method
-    "#{API_ENDPOINT}/#{resource}/#{method}"
+    def initialize(code)
+      @code = code
+    end
+  end
+
+  class UnknownError < Echorb::Error; end
+  class InvalidAPIKeyError < Echorb::Error; end
+  class UnauthorizedAPIKeyError < Echorb::Error; end
+  class RateLimitError < Echorb::Error; end
+  class MissingParamError < Echorb::Error; end
+  class InvalidParamError < Echorb::Error; end
+
+  class ErrorFactory
+    def self.create(code)
+      klass = case code
+      when -1 then Echorb::UnknownError
+      when  1 then Echorb::InvalidAPIKeyError
+      when  2 then Echorb::UnauthorizedAPIKeyError
+      when  3 then Echorb::RateLimitError
+      when  4 then Echorb::MissingParamError
+      when  5 then Echorb::InvalidParamError
+      end
+
+      klass.new(code)
+    end
   end
 end
