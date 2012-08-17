454962895caf6b8e74b845c485644e140b87881b
Eduardo Lanchares <edulanchi@gmail.com>
2012-08-17 08:48:39 +0200

    Added some helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,26 @@
-require 'json'
-require 'webmock/rspec'
-require 'echorb'
+require "echorb"
+require "webmock/rspec"
+
+def a_get(path, endpoint='http://developer.echonest.com/api/v4')
+  a_request(:get, endpoint + path)
+end
+
+def a_post(path, endpoint='http://developer.echonest.com/api/v4')
+  a_request(:post, endpoint + path)
+end
+
+def stub_get(path, endpoint='http://developer.echonest.com/api/v4')
+  stub_request(:get, endpoint + path)
+end
+
+def stub_post(path, endpoint='http://developer.echonest.com/api/v4')
+  stub_request(:post, endpoint + path)
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(File.join(fixture_path, file))
+end
